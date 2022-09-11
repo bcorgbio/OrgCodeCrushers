21958aaad86324b6e2ef144df103e0044e05a416
wilkenj14 <112563878+wilkenj14@users.noreply.github.com>
2022-09-11 18:40:19 -0400

    Jenna M1P1

--- Jenna-Project1.R
+++ Jenna-Project1.R
@@ -2,14 +2,6 @@ library(ggplot2)
 library(tidyverse)
 setwd("~/Desktop/BIOL3140/OrgCodeCrushers/Project1/ModProj1- Jenna")
 dat <- read.csv("scales.csv")
-dim(dat)
-mean(dat$N)
-sapply(dat,class)
-dat$species <- as.factor(dat$species)
-species <- levels(dat$species)
-species
-dat$species==species[1]
-dat$species[dat$species==species[1]]
 dat %>%
   group_by(species) %>%
   summarise(n = n())
