0c64cf71b0a20ef6ab677e6480b71f2dcc4335f8
brendanfox12 <foxbt@bc.edu>
2022-09-18 20:31:04 -0400

    Tingwei Project 2 Submission

--- TingweiHu_Project2.R
+++ TingweiHu_Project2.R
@@ -1,6 +1,6 @@
 #F22 BIOL3140 Project Two
+#1
 library(tidyverse)
-library(ggplot2)
 library(features) #assuming packages already installed
 pseed <- read_csv("pseed.fin.amps.csv")
 pseed.bl <- read_csv("pseed.lengths.csv")
@@ -13,16 +13,51 @@ pseed.wide <- pseed%>%
   mutate(amp.sum=sum(amp.bl)) %>% 
   select(-amp)%>%
   pivot_wider(names_from = fin,values_from = amp.bl) %>%
-  mutate(amp.sum=L+R)
-#the code above created pseed.wide
-exp1 <- pseed.wide %>% 
-  filter() %>% 
+  mutate(amp.sum=L+R) %>% 
+  print() #the code above created pseed.wide
+          #print() was included at every "checkpoint" to visualize result
+
+#2
+find.peaks <- function(x,y,mult=100){
+  f <- fget(features(x = x,y=y*mult))[2:3]%>% 
+    as_tibble()%>% 
+    filter(curvature<0)%>% 
+    mutate(peaks=round(crit.pts,0))
+  return(f$peaks)
+}
+
+pseed.peaks <- pseed.wide%>%
+  group_by(fish,bl.s)%>%
+  mutate(peak=frame %in% find.peaks(frame,amp.sum))%>%
+  filter(peak==TRUE)
+
+pseed.sum.max<- pseed.peaks%>%
+  group_by(fish,bl.s)%>%
+  summarize(amp.sum.mean=mean(amp.sum)) %>% 
   print()
-f1 <- features(x = exp1$frame, y=exp1$amp.sum*100)
-fget(f1)
-fish.speed <- pseed.wide %>% 
-  ungroup() %>% 
+
+#3 
+sem = function(x){
+  sd(x)/sqrt(length(x))
+}
+
+pseed.sum.max <- pseed.peaks %>% 
   group_by(fish,bl.s) %>% 
-  summarize
-fish.speed$num <- seq(1:12)
-fish.speed
+  summarise(amp.sum.se=sem(amp.sum)) %>% 
+  left_join(pseed.sum.max, by = c('fish','bl.s')) %>% 
+  print()
+
+#4
+pseed.sum.max %>% 
+  ggplot(aes(x=bl.s,y=amp.sum.mean,col=fish)) + geom_point() + geom_line() +
+  geom_errorbar(aes(ymin=amp.sum.mean-amp.sum.se,ymax=amp.sum.mean+amp.sum.se))
+
+#5
+met.specs <- read_csv('pseed.met.rate.csv')
+met.n.amp <- pseed.sum.max %>% 
+  left_join(met.specs, by = c('bl.s','fish')) %>% 
+  print()
+
+#6
+met.n.amp %>% 
+  ggplot(aes(x=met.rate,y=amp.sum.mean,col=fish)) + geom_point()
