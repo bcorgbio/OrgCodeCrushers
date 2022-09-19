e57f67e30f3bfc5bcef599f7bc2b2d26887f9811
brendanfox12 <foxbt@bc.edu>
2022-09-19 10:49:25 -0400

    ClassNotes

---
+++ 9:19_ClassNotes_Fox.R
@@ -0,0 +1,13 @@
+#Left side is predicted by ~ Right side
+library(tidyverse)
+
+set.seed(123)
+x.A=1:50
+y.A=x.A*2+runif(50,1,200)
+x.B=1:50
+y.B=x.B*3.5+runif(50,1,200)
+d <- tibble(x=c(x.A,x.B), y=c(y.A,y.B), species = c(rep("A",50),rep("B",50)))
+
+lm(y~x*d)
+#Difference in intercept --> x+d
+#Difference in slope --> x*d
